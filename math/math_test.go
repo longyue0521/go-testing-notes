f49dfe17248aefaf289ee7e353818041713c8cbe
longyue0521 <longyueli0521@gmail.com>
2020-10-08 15:38:32 +0800

    remove FAIL info in t.Errorf

--- math/math_test.go
+++ math/math_test.go
@@ -9,13 +9,13 @@ func TestSum(t *testing.T) {
 	sum := Sum([]int{0, 1, 2})
 	if sum != 3 {
 		// 个性化提示信息，以便快速找到测试用例
-		t.Errorf("FAIL 1: Wanted 3 but Got %d", sum)
+		t.Errorf("Wanted 3 but Got %d", sum)
 	}
 
 	// 测试用例2
 	sum = Sum([]int{10, -1, 2})
 	if sum != 11 {
 		// 个性化提示信息，以便快速找到测试用例
-		t.Errorf("FAIL 2: Wanted 11 but Got %d", sum)
+		t.Errorf("Wanted 11 but Got %d", sum)
 	}
 }
